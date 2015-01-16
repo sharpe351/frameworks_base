d844044db5094c05504fb3a144453ed04e6314c2
Dave Kessler <activethrasher00@gmail.com>
2015-01-15 20:12:07 -0500

    already had this feature: "Quick settings: Option to disable brightness slider (1/2)"
    This reverts commit 3918ce591b83dc35f53577b17fad9a5041bd883b.
    
    Change-Id: Ie57eacc7e04f42bb36071fb54b54f7d31b54159b

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -24,9 +24,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Handler;
-import android.os.UserHandle;
 import android.os.Message;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -114,25 +112,6 @@ public class QSPanel extends ViewGroup {
                 closeDetail();
             }
         });
-    }
-
-    /**
-     * Enable/disable brightness slider.
-     */
-    private boolean showBrightnessSlider() {
-        boolean brightnessSliderEnabled = Settings.System.getIntForUser(
-            mContext.getContentResolver(), Settings.System.QS_SHOW_BRIGHTNESS_SLIDER,
-                1, UserHandle.USER_CURRENT) == 1;
-        ToggleSlider brightnessSlider = (ToggleSlider) findViewById(R.id.brightness_slider);
-        if (brightnessSliderEnabled) {
-            mBrightnessView.setVisibility(VISIBLE);
-            brightnessSlider.setVisibility(VISIBLE);
-        } else {
-            mBrightnessView.setVisibility(GONE);
-            brightnessSlider.setVisibility(GONE);
-        }
-        updateResources();
-        return brightnessSliderEnabled;
     }
 
     private void updateDetailText() {
@@ -229,7 +208,7 @@ public class QSPanel extends ViewGroup {
         if (mListening) {
             refreshAllTiles();
         }
-        if (listening && showBrightnessSlider()) {
+        if (listening) {
             mBrightnessController.registerCallbacks();
         } else {
             mBrightnessController.unregisterCallbacks();
@@ -414,7 +393,7 @@ public class QSPanel extends ViewGroup {
                 tileRecord.tileView.setVisibility(newVis);
             }
         }
-        mBrightnessView.setVisibility(showBrightnessSlider() ? VISIBLE : GONE);
+        mBrightnessView.setVisibility(newVis);
         mGridContentVisible = visible;
     }
 
