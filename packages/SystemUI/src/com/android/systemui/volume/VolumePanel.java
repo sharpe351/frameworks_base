418640681c0ac7c45c96126fce3c5c766ef951f8
Dave Kessler <activethrasher00@gmail.com>
2015-02-15 23:47:05 -0500

    VolumePanel: add forgotten variable
    Change-Id: Ic5e4ab62fa15bb21d50a2fda1519f0dad7d4bd9b

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -101,6 +101,7 @@ public class VolumePanel extends Handler {
     private static final int TIMEOUT_DELAY_COLLAPSED = 4500;
     private static final int TIMEOUT_DELAY_SAFETY_WARNING = 5000;
     private static final int TIMEOUT_DELAY_EXPANDED = 10000;
+    private static final int ANIMATION_DURATION = 350; // ms
 
     private static final int MSG_VOLUME_CHANGED = 0;
     private static final int MSG_FREE_RESOURCES = 1;
